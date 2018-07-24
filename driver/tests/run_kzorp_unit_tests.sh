9e8e061dc93c2905bbc052b8936c3514bd8c9b4f
Baranyai Balázs <baranyai.balazs@balasys.hu>
2018-07-24 10:29:39 +0200

    driver/tests/run_kzorp_unit_tests.sh: minor fixes
    References: jira ZORP-1115

--- driver/tests/run_kzorp_unit_tests.sh
+++ driver/tests/run_kzorp_unit_tests.sh
@@ -18,7 +18,7 @@ function print_help(){
 "   -b | --branch BRANCH - branch name of the repository where kZorp is compiled from \n" \
 "   -a | --arch ARCHITECTURE - Architecture name of the package to be installed\n" \
 "   -v | --version VERSION - Ubuntu version to run the test with\n" \
-"   -p | --path PATH - Path of the tests directory\n"
+"   -p | --path PATH - Path of the tests directory\n" \
 "   -h | --help - Display this information \n"
 }
 
@@ -69,7 +69,7 @@ OSVersion="18.04"
 while (( $# )); do
   case $1 in
     "-k" | "--kmemleak") KMemLeakImage="$2"; shift 2;;
-    "-r" | "--Repository") Repository="$2"; shift 2;;
+    "-r" | "--repository") Repository="$2"; shift 2;;
     "-b" | "--branch") Branch="$2"; shift 2;;
     "-a" | "--arch") Architecture="$2"; shift 2;;
     "-v" | "--version") OSVersion="$2"; shift 2;;
